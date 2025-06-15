3bb0994b9622d014307e82822d525f6345066063
rmsid1312 <rmsid1312@naver.com>
2025-06-15 13:48:21 +0900

    #2 Apply composition

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -54,19 +54,26 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox movies={movies}>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
     <>
       <nav className="nav-bar">
         <Logo />
-        <Search />
-        <NumResults movies={movies} />
+        {children}
       </nav>
     </>
   );
@@ -107,18 +114,15 @@ function Search() {
   );
 }
 
-function Main({ movies }) {
+function Main({ children }) {
   return (
     <>
-      <main className="main">
-        <ListBox movies={movies} />
-        <WatchedBox />
-      </main>
+      <main className="main">{children}</main>
     </>
   );
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -130,7 +134,7 @@ function ListBox({ movies }) {
         >
           {isOpen1 ? "–" : "+"}
         </button>
-        {isOpen1 && <MovieList movies={movies} />}
+        {isOpen1 && children}
       </div>
     </>
   );
