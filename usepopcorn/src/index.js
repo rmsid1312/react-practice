8d67650bd949abf3bee3097af986b2041afcee7a
rmsid1312 <rmsid1312@naver.com>
2025-06-15 15:10:26 +0900

    #7 props type 검사 추가

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movies was rated {movieRating} starts</p>
+    </>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={10} size={24} color="red" className="test" />
+    <Test />
   </React.StrictMode>
 );
