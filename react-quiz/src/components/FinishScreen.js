7742616463166be097d148043761801bd6f6d8b4
rmsid1312 <rmsid1312@naver.com>
2025-07-12 22:31:50 +0900

    section16 timer

---
+++ react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,22 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  return (
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(HighScore: {highscore} points)</p>
+
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
