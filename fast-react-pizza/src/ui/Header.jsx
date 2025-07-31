7764545a4ab24e7ba825036b53efb64a084a45a8
rmsid1312 <rmsid1312@naver.com>
2025-08-01 08:36:44 +0900

    section 23-297

---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
+
+function Header() {
+  return (
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-[5px]">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <Username />
+    </header>
+  );
+}
+
+export default Header;
