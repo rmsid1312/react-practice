f91cc18facb86857408670a00a12c47874a0bad7
rmsid1312 <rmsid1312@naver.com>
2025-08-20 23:41:15 +0900

    section 27 - react query update + get for settings

---
+++ the-wild-oasis/src/features/settings/useUpdateSetting.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+import toast from "react-hot-toast";
+
+export default function useUpdateSetting() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      toast.success("Setting is successfully updated");
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return { isUpdating, updateSetting };
+}
--- the-wild-oasis/src/services/apiSettings.js
+++ the-wild-oasis/src/services/apiSettings.js
@@ -1,27 +1,39 @@
-import supabase from "./supabase";
+import api from "./api";
 
 export async function getSettings() {
-  const { data, error } = await supabase.from("settings").select("*").single();
-
-  if (error) {
-    console.error(error);
-    throw new Error("Settings could not be loaded");
+  try {
+    const res = await api.get("/settings");
+    return res.data;
+  } catch (err) {
+    console.error(err?.response?.data || err.message);
+    throw new Error("Cabins could not be loaded.");
   }
-  return data;
+}
+
+export async function updateSetting(newValue) {
+  try {
+    // const res = await api.put(`/settings/${id}`, data);
+    console.log("newValue : ", newValue);
+    const res = await api.put(`/settings/1`, newValue);
+    return res.data;
+  } catch (err) {
+    console.log("failed to edit settings", err?.response?.data || err.message);
+    throw err;
+  }
 }
 
 // We expect a newSetting object that looks like {setting: newValue}
-export async function updateSetting(newSetting) {
-  const { data, error } = await supabase
-    .from("settings")
-    .update(newSetting)
-    // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
-    .eq("id", 1)
-    .single();
+// export async function updateSetting(newSetting) {
+//   const { data, error } = await supabase
+//     .from("settings")
+//     .update(newSetting)
+//     // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
+//     .eq("id", 1)
+//     .single();
 
-  if (error) {
-    console.error(error);
-    throw new Error("Settings could not be updated");
-  }
-  return data;
-}
+//   if (error) {
+//     console.error(error);
+//     throw new Error("Settings could not be updated");
+//   }
+//   return data;
+// }
